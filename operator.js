0f86f8799d3c92b3ef0f9a1962603b0c0d267180
Naveenap24 <naveena24008@gmail.com>
2026-06-12 11:37:16 +0530

    operator.js

--- operator.js
+++ operator.js
@@ -7,7 +7,11 @@ console.log (a*b);//multiplication
 console.log (a/b);//divison
 console.log (a%b);//modulus
 console.log (a**b);//exponentiation
-
-
-
+//assignment
+console.log ("assignment operator")
+var c=30;
+console.log(c+=10);
+console.log(c-=10);
+console.log(c*=10);
+console.log(c/=10);
 
