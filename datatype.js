574bab172b654e398b53343f6e2c81d0fb024d6a
Naveenap24 <naveena24008@gmail.com>
2026-06-12 15:53:23 +0530

    function.js

---
+++ datatype.js
@@ -0,0 +1,54 @@
+console.log ("Welcome to primitive data types")
+
+//numbers
+console.log ("DT numbers");
+var age =30;
+console.log (age);
+
+//string
+console.log ("DT strings");
+var name = "john";
+console.log (name);
+
+//booleans
+console.log("DT Booleans");
+var isstudent =true;
+console.log (isstudent);
+
+//null
+console.log ("DT Null");
+var emptyValue = null;
+console.log(emptyValue);
+
+//undefined
+console.log ("DT undefined");
+var undefinedValue;
+console.log(undefinedValues);
+
+//symbols
+console.log ("DT symbols");
+var symbolValue =symbol ("symbol");
+console.log(symbolValue);
+
+//BigInt
+console.log("DT BigInt");
+var BigInt=12345678901234567890123456789012467899932335475877382652424354747574635353535335
+console.log(BigIntValue);
+
+console.log("welcome to non primitive data types")
+
+//objects
+console.log ("DT objects");
+var person = {
+    name:"naveena",
+    age =30;
+};
+console.log(person);
+console.log(person.age);
+
+//arrays
+console.log("DT array");
+var fruits = ["kiwi","fig","pine","apple","orange"];
+console.log(fruits.length);
+console.log(fruits[4]);
+
---
+++ function.js
@@ -0,0 +1,14 @@
+console.log("FUNCTION")
+
+function great()
+{
+    console.log("hello world")
+}
+great()
+
+console.log("PARAMETIC")
+function add(a,b)
+{
+    console.log(a+b)
+}
+add(4,7)
