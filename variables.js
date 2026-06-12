30ca5c5ec0597908a9a589681de21917f2639903
Naveenap24 <naveena24008@gmail.com>
2026-06-12 10:25:38 +0530

    variables example

---
+++ variables.js
@@ -0,0 +1,13 @@
+//variable declaration using var 
+var name = "Vellalar"
+console.log(name)
+
+//variable declaration using let
+{
+let age = 25
+console.log(age)
+}
+
+//variable declaration using const
+const country ="india"
+console.log(country)
