554150725b67c166defdf15a29180e23a55b15a8
Naveenap24 <naveena24008@gmail.com>
2026-06-12 11:14:52 +0530

    operator.js:

---
+++ operator.js
@@ -0,0 +1,13 @@
+//arthimetic
+var a=10;
+var b=20;
+console.log (a+b);//addition
+console.log (a-b);//subtraction
+console.log (a*b);//multiplication
+console.log (a/b);//divison
+console.log (a%b);//modulus
+console.log (a**b);//exponentiation
+
+
+
+
