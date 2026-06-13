4cf5c4b9845555a649bac2bf2538b8bd87e229f8
Naveenap24 <naveena24008@gmail.com>
2026-06-13 11:58:47 +0530

    looping.js

---
+++ looping.js
@@ -0,0 +1,27 @@
+// For Loop
+console.log("Two Table");
+console.log("For Loop");
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i + " * 2 = " + (i * 2));
+}
+
+// While Loop
+console.log("While Loop");
+
+let i = 1;
+
+while (i <= 10) {
+    console.log("2 * " + i + " = " + (2 * i));
+    i++;
+}
+
+// Do While Loop
+console.log("Do While Loop");
+
+let j = 1;
+
+do {
+    console.log("2 * " + j + " = " + (2 * j));
+    j++;
+} while (j <= 10);
